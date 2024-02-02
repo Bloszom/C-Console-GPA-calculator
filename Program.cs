5be20232d395c6da62c4180c446d1eb0f19e423a
Bloszom <blossom.giane@decagon.dev>
2024-02-03 00:14:03 +0100

    Update on the code for  the calcualation and for the printing of student records into the table format

--- Program.cs
+++ Program.cs
@@ -13,13 +13,13 @@ namespace Blossom_WeekOne_Task
             Console.WriteLine("Please enter your registered Number of courses: ");
             int registeredNumberOfCourses = int.Parse(Console.ReadLine());
 
-             double StudentGpa;
+             double studentGpa;
              string studentGrade;
              string studentRemark;
              int studentGradeUnit;
              int studentWeightPt;
              int studentTotalGradeUnit = 0;
-             int studentTotalWeightPoint = 0;
+             int studentTotalWeightPt = 0;
              int studentTotalUnitPassed = 0;
              int studentTotalCourseUnit = 0;
 
@@ -27,11 +27,11 @@ namespace Blossom_WeekOne_Task
 
             for (int i = 0; i < registeredNumberOfCourses; i++)
             {
-                Console.WriteLine($"Enter Course Code");
+                Console.WriteLine($"{i + 1}Enter Course Name and Code (For eaxmple: (mth 101)):");
                 var courseCode = Console.ReadLine();
-                Console.Write($"Enter Course Unit: ");
+                Console.Write($"{i + 1}Enter Course Unit: ");
                 int courseUnit = int.Parse(Console.ReadLine());
-                Console.Write($" Enter Course Score: ");
+                Console.Write($"{i + 1}Enter Course Score: ");
                 int courseScore = int.Parse(Console.ReadLine());
 
                 studentGrade = (courseScore >= 70 && courseScore <= 100) ? "A" :
@@ -54,9 +54,48 @@ namespace Blossom_WeekOne_Task
 
                 studentWeightPt = courseUnit * studentGradeUnit;
 
-               
+                if (courseScore >= 40)
+                {
+                    studentTotalUnitPassed += courseUnit;
+                }
+                studentTotalCourseUnit += courseUnit;
+                studentTotalWeightPt += studentWeightPt;
+                studentTotalGradeUnit += studentGradeUnit;
 
+
+                string[] courseRecord = new string[] { courseCode,
+                                                        courseUnit.ToString(),
+                                                            studentGrade,
+                                                                studentGradeUnit.ToString(),
+                                                                    studentWeightPt.ToString(),
+                                                                       studentRemark };
+
+
+                studentRecords.Add(courseRecord);
             }
+
+                studentGpa = studentTotalWeightPt / studentTotalGradeUnit;
+
+
+                Console.WriteLine(" |------------------|-----------------|-----------|---------------|---------------|--------------|");
+                Console.WriteLine(" | COURSE & CODE    | COURSE UNIT     | GRADE     | GRADE-UNIT    | WEIGHT Pt     | REMARK       |");
+                Console.WriteLine(" |------------------|-----------------|-----------|---------------|---------------|--------------|");
+                foreach (string[] courseRecord in studentRecords)
+                {
+                    Console.WriteLine($" |    {courseRecord[0],-14}|  " +
+                                         $"   {courseRecord[1],-12}|  " +
+                                         $"   {courseRecord[2],-6}|  " +
+                                         $"  {courseRecord[3],-11}| " +
+                                         $"   {courseRecord[4],-11}| " +
+                                         $"   {courseRecord[5],-10}|");
+                }
+
+                Console.WriteLine("\n\n");
+                Console.WriteLine($"Total course Unit registered is {studentTotalCourseUnit}");
+                Console.WriteLine($"Total course Unit passed is {studentTotalUnitPassed}");
+                Console.WriteLine($"Total Weight point is {studentTotalWeightPt}");
+                Console.WriteLine($"GPA is {studentGpa:F2}");
+ 
 
         }
     }
